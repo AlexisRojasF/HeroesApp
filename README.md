84dccdbb76725984ac49fa1e138e91efa40d85d8
AlexisRojasF <alexisrojasf@outlook.es>
2021-05-20 12:18:18 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# HeroApp
+
+aplicacion de react sobre heroes de Dc y Marvel usando Rutas puiblicas y privadas
+
+URL PAGES:
---
+++ docs/precache-manifest.65c8fafac9762d8310c544833622a524.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "add5ff78e04387f99ca1575c477eb09a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a61565465a1262941301",
+    "url": "/static/js/2.b0ad0e2c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b0ad0e2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e978d1946ac96e9c8468",
+    "url": "/static/js/main.bc002649.chunk.js"
+  },
+  {
+    "revision": "ede06adfc8d41a12f0e7",
+    "url": "/static/js/runtime-main.c607270d.js"
+  }
+]);
